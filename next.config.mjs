864f9f072b95d9f314ecdd23349a75a496c9650d
Fairoz007 <fairozfaisal2001@gmail.com>
2026-08-09 21:39:01 +0400

    feat: add OpenNext Cloudflare support and deployment configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
   },
 }
+
+initOpenNextCloudflareForDev()
 
 export default nextConfig
